bb17bba4b0e04a0d7a72d082997431bf45e2b3f4
KatWint <92839131+KatWint@users.noreply.github.com>
2022-02-24 05:44:02 -0800

    Assignment using queries, first commit

---
+++ daikichiroutes/src/main/java/com/katwdojo/daikichiroutes/controllers/DaikichiController.java
@@ -0,0 +1,40 @@
+package com.katwdojo.daikichiroutes.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController 
+@RequestMapping("/daikichi")
+
+public class DaikichiController {
+
+	@RequestMapping("")
+	public String welcome() {
+		return "Welcome";
+	}
+	
+	@RequestMapping("/today")
+	public String luckToday() {
+		return "Today you will find luck in your endeavors!!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String luckTomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travelCity(@PathVariable("city") String city) {
+		return "Congratulations!!! You will soon travel to " + city;
+	}
+	
+	@RequestMapping("/lotto/{number}")
+		public String lotto(@PathVariable(value = "number") Integer number) {
+			if (number % 2==0) {
+				return "You will take a grand adventure journey in the near future, but be weary of tempting offers.";
+			}else return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+	
+	}
+}
+
